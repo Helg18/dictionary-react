7d07caab526439c3ab6becf06db9b03deceb69d8
Henry Leon <helg18@gmail.com>
2025-03-10 14:07:00 -0400

    Invoke service to get a sample word

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
+import dictionaryAPI from './services/dictionaryAPI';
 import './App.css';
 
 function App() {
+  const [wordData, setWordData] = useState(null); // Estado para almacenar los datos de la palabra
+
+  useEffect(() => {
+    async function fetchWord() {
+      try {
+        const data = await dictionaryAPI.getWord('apple');
+        setWordData(data);
+      } catch (error) {
+        console.error("Error fetching word:", error);
+      }
+    }
+
+    fetchWord();
+  }, []); // El array vacío asegura que el efecto se ejecute solo una vez al montar el componente
+
   return (
     <div className="App">
       <header className="App-header">
